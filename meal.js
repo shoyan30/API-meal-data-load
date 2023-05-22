fa8e452d52a9091b043f119c49a3e4248aa0ebf9
FIRST_NAME LAST_NAME <mdshoyan30@gmail.com>
2023-05-23 03:09:06 +0600

    Meal-data-loaded

---
+++ meal.js
@@ -0,0 +1,46 @@
+function searchFood(){
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+
+    searchField.value = "";
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s= ${searchText}`;
+
+    //console.log(url);
+
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data => displaySearchResult(data.meals))
+
+}
+
+function displaySearchResult(meals){
+
+    //console.log(meal);
+
+    const searchResult = document.getElementById('search-result');
+     for(const meal of meals){
+        console.log(meal);
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+
+        div.innerHTML =`
+        <div class="card h-100">
+            <img src="${meal.strMealThumb
+            }" class="card-img-top" alt="...">
+            <div class="card-body">
+               <h5 class="card-title">${meal.strMeal
+               }</h5>
+               <p class="card-text">${meal.strInstructions.slice(0, 250)
+               }</p>
+            </div>
+        </div>
+        
+        `;
+        searchResult.appendChild(div);
+     }
+
+}
+
